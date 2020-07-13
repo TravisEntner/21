508a872e5791b2b37134e95ecb356fd62846e736
TravisEntner <Travisentner@gmail.com>
2020-07-13 19:00:59 +0000

    debugged

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 print("This is 21, how you play is, i will give you 2 random numbers, your goal is to get as close to 21 as posible without going over, also your total must be higher than mine, but you wont know what my numbers are")
-firstNum = random.randint(1,12)
-secondNum = random.randint(1,12)
-myFirst = random.randint(1,12)
-mySecond = random.randint(1,12)
+firstNum = random.randint(1,10)
+secondNum = random.randint(1,10)
+myFirst = random.randint(1,10)
+mySecond = random.randint(1,10)
 myTotal = myFirst + mySecond
 print("Your first number is, " + str(firstNum) + " and your second number is, " + str(secondNum))
 
@@ -11,30 +11,26 @@ usertotal = firstNum + secondNum
 print("Your total is " + str(usertotal))
 if(usertotal == 21):
   print("You got a blackjack you win")
-
+elif(usertotal > 21):
+  print("Your card are over 21 you lost")
 
 ask = input("Are you happy with you numbers?(If you are not happy with your total I will give you a new number to add to the total)  yes or no, please do not use caps: ")
-
-while(ask == "no"):
-  newNum = random.randint(1,12)
-  usertotal = usertotal + newNum
-  print("your new total is " + usertotal)
-  askAgain = input("Are you happy with you numbers?(If you are not happy with your total I will give you a new number to add to the total)  yes or no, please do not use caps: ")
-  if(ask == yes and usertotal > 21):
-    print("you lose your total is over 21")
-  elif(ask == "yes" and usertotal > myTotal):
-      print("Your total was bigger than mine, you win, my number is " + str(myTotal) )
-  elif(ask == "yes" and usertotal < myTotal):
-      print("My number was bigger than yours I win, my number was " + str(myTotal))
-
-if(ask == yes):
-  elif(ask == "yes" and usertotal > 21):
-    print("you lose your total is over 21")
-  elif(ask == "yes" and usertotal > myTotal):
-    print("Your total was bigger than mine, you win, my number is " + str(myTotal) )
-  elif(ask == "yes" and usertotal < myTotal):
-    print("My number was bigger than yours I win, my number was " + str(myTotal))
-
-
-
-
+for n in range(21):
+  if(ask == "no"):
+    newNum = random.randint(1,10)
+    usertotal = usertotal + newNum
+    print("your new total is " + str(usertotal))
+  if(ask == "no" and usertotal > 21):
+    print("Oh no you lost your total is over 21.")
+  if(ask =="no" and usertotal < 21):
+    ask = input("Are you happy with you numbers?(If you are not happy with your total I will give you a new number to add to the total)  yes or no, please do not use caps: ")
+  if(ask == "yes" and usertotal > 21):
+    print("Oh no you lost your total was bigger than")
+  if(ask == "yes" and myTotal > usertotal):
+    print("Oh no You lost,because my total was bigger than yours, my total was " + str(myTotal))
+  if(ask == "yes" and usertotal > myTotal and usertotal <= 21 or usertotal == 21):
+    print("You win, my total was " + str(myTotal))
+  if(usertotal > 21 or usertotal == 21):
+    break
+  if(ask == "yes"):
+    break
